4a096bf427fbdacad070bcf1c425609e36f8d8c1
karanpawar01 <pawarkaran9950@gmail.com>
2026-08-13 12:32:57 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,14 @@
+name=input("Enter your name")
+print("hello", name)
+
+age=input("Enter your age")
+print("age is"  , age)
+
+weight=input("Enter your weight")
+print("weight is"  , weight)
+
+college=input("Enter your college")
+print("college is"  , college)
+
+branch=input("Enter your branch")
+print("branch is"  , branch)
